ec696c4b677d12196b4ce2ffc0012a78f8e0c061
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-06-02 20:17:50 +0100

    create layout, add header and footer

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header>
+        <img src="./src/assets/images/apple-touch-icon.png" alt="logo" />
+        <h1>Local Weather</h1>
+      </header>
+      <main>
+        <section></section>
+        <section></section>
+      </main>
+      <footer>© Maciej Skurznica, 2023</footer>
+    </>
+  );
+}
+
+export default App;
