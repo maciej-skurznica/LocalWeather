73a82278fb38cd763a562d0857e3f86347673efe
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-06-02 17:16:59 +0100

    add prettier & prettier organize imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true,
+    node: true
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
-  },
+    "react-refresh/only-export-components": "warn"
+  }
 };
