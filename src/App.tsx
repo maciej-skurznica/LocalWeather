623a277cee324c4b8b27a3f94ed65faf2e260b4d
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-06-05 12:16:19 +0100

    change the logo image file

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
   return (
     <>
       <header>
-        <img src="./src/assets/images/favicon.ico" alt="logo" />
+        <img src="./public/logo.png" alt="logo" />
         <h1>Local Weather</h1>
       </header>
       <main>
